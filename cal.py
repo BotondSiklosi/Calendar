2f818b04886a9462581e6211024873e0c03d77b4
BotondSiklosi <kissiki99@gmai.com>
2019-05-24 21:31:12 +0200

    cancel existing meeting function

--- cal.py
+++ cal.py
@@ -5,17 +5,28 @@ import ui
 import storage
 # saving and loading files
 
+MEETING_TITLE = 0
+HOW_LONG = 1
+STARTING_TIME = 2
+
 
 def schedule_new_meeting(table):
     labels_title = ["Enter meeting title", "Enter duration in hours (1 or 2)", "Enter start time"]
-    items_to_add = ui.get_input(labels_title, 'Your choise: s')
+    items_to_add = ui.get_input(labels_title, "Schedule a new meeting.")
     table.append(items_to_add)
     storage.write_file_from_table("meetings.txt", table)
     return table
 
 
 def cancel_existing_meeting(table):
-    pass
+    labels_title = ["Enter the start time"]
+    inputs = ui.get_input(labels_title, "Cancel an existing meeting.")
+    item_to_delete = inputs[0]
+    for i in table:
+        if i[STARTING_TIME] == item_to_delete:
+            table.remove(i)
+    storage.write_file_from_table("meetings.txt", table)
+    return table
 
 
 def handle_menu():
@@ -27,7 +38,7 @@ def main_menu():
     table = storage.get_table_from_file("meetings.txt")
     ui.print_table("Your schedule for the day", table)
     handle_menu()
-    inputs = ui.get_input(["Please chose an option"], "")
+    inputs = ui.get_input(["Your choise"], "")
     options = inputs[0]
     if options == "s":
         schedule_new_meeting(table)
